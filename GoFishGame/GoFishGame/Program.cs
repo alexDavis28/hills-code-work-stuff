6d412238c46f105227a7d1b9809bd1358ae05006
8m3xl <8m3xlt@gmail.com>
2022-03-20 18:32:39 +0000

    ee

--- GoFishGame/GoFishGame/Program.cs
+++ GoFishGame/GoFishGame/Program.cs
@@ -8,7 +8,7 @@ namespace GoFishGame
         static void Main(string[] args)
         {
             // Create a game with 1 human player and 2 ai players
-            GoFish game = new GoFish(1, 2);
+            GoFish game = new GoFish(0, 3);
             game.Deal();
             game.Play();
         }
