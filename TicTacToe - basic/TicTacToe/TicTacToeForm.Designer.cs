881055076151d15d6268536048a72622cbb19a0e
8m3xl <8m3xlt@gmail.com>
2022-04-20 12:35:12 +0100

    Tic tac toe game

---
+++ TicTacToe - basic/TicTacToe/TicTacToeForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Noughts and Crosses";
+            // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(243, 574);
+            this.newGameButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(100, 28);
+            this.newGameButton.TabIndex = 1;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 615);
+            this.Controls.Add(this.newGameButton);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "TicTacToeForm";
+            this.Text = "Noughts and Crosses";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button newGameButton;
+    }
+}
+
---
+++ TicTacToe - basic/TicTacToe/TicTacToeForm.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToeForm : Form
+    {
+        private string NoughtOrCross;
+        private Button[,] gridButtons;
+
+        // Grid
+        const int SQUARE_GRID_SIZE = 3;
+        const int cols = SQUARE_GRID_SIZE;
+        const int rows = SQUARE_GRID_SIZE;
+        const int gridWidth = 300;
+        const int gridHeight = 300;
+        const int tileWidth = gridWidth / cols;
+        const int tileHeight = gridHeight / rows;
+        const int gridTop = 100;
+        const int gridLeft = 50;
+
+        private void NewGame()
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    gridButtons[r, c].Text = "";
+                    gridButtons[r, c].Enabled = true;
+                    gridButtons[r, c].BackColor = Color.Beige;
+                }
+            }
+            NoughtOrCross = "X";
+        }
+
+        /// <summary>
+        /// Generates a grid of buttons  
+        /// </summary>
+        /// <param name="rows">Numebr of rows</param>
+        /// <param name="cols">Number of columns</param>
+        /// <param name="tileWidth">Button Width</param>
+        /// <param name="tileHeight">Button Height</param>
+        /// <param name="gridTop">Vertical position</param> 
+        /// <param name="gridLeft">Horizontal position</param>
+        private void CreateGrid(int rows, int cols, int tileWidth, int tileHeight, int gridTop, int gridLeft)
+        {
+            gridButtons = new Button[rows, cols];
+            // double for loop to handle all rows and columns
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    // create new Button control which will be one 
+                    // of the buttons of the grid
+                    gridButtons[r, c] = new Button()
+                    {
+                        Size = new Size(tileWidth, tileHeight),
+                        Location = new Point(tileWidth * c + gridLeft, tileHeight * r + gridTop),
+                        BackColor = Color.Beige,
+                        Font = new Font("Arial", gridWidth / cols / 3), // font size proportional to width and # columns
+                    };
+                    // you can also modify the properties 
+                    gridButtons[r, c].ForeColor = Color.Black;
+                    // add to Form's Controls so that they show up
+                    gridButtons[r, c].Click += new EventHandler(gridButtonClick);
+                    // 
+                    Controls.Add(gridButtons[r, c]);
+                }
+            }
+
+        }
+
+
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CreateGrid(rows, cols, tileWidth, tileHeight, gridTop, gridLeft);
+            NewGame();
+
+        }
+        private void gridButtonClick(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "")
+            {
+                ((Button)sender).Text = NoughtOrCross;
+
+                if (CheckWin())
+                {
+                    MessageBox.Show($"{NoughtOrCross} wins!");
+                    NewGame();
+                }
+                else
+                {
+                    if (NoughtOrCross == "X")
+                    {
+                        NoughtOrCross = "O";
+                    }
+                    else
+                    {
+                        NoughtOrCross = "X";
+                    }
+                }
+            }
+            
+        }
+
+        private bool CheckWin()
+        {
+            bool result = false;
+            for (int r = 0; r < rows; r++)
+            {
+                if (gridButtons[r, 0].Text == gridButtons[r, 1].Text && gridButtons[r,0].Text == gridButtons[r, 2].Text && gridButtons[r, 0].Text != "")
+                {
+                    result = true;
+                }
+            }
+            for (int c = 0; c < cols; c++)
+            {
+                if (gridButtons[0, c].Text == gridButtons[1, c].Text && gridButtons[1, c].Text == gridButtons[2, c].Text && gridButtons[0, c].Text != "")
+                {
+                    result = true;
+                }
+            }
+            if (gridButtons[0, 0].Text == gridButtons[1, 1].Text && gridButtons[1, 1].Text == gridButtons[2, 2].Text && gridButtons[0, 0].Text != "")
+            {
+                result = true;
+            }
+            if (gridButtons[0, 2].Text == gridButtons[1, 1].Text && gridButtons[1, 1].Text == gridButtons[2, 0].Text && gridButtons[0, 2].Text != "")
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+    }
+    
+}
