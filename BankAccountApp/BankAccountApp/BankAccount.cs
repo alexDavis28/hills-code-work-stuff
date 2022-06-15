273f9cf8df64714a8861ba609b1892a7db0a9742
8m3xl <8m3xlt@gmail.com>
2022-06-15 12:06:32 +0100

    eeeee

---
+++ BankAccountApp/BankAccountApp/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountApp
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount()
+        {
+        }
+
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Add(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+
+            balance -= amount;
+        }
+
+        public void TransferFundsTo(BankAccount otherAccount, double amount)
+        {
+            if (otherAccount is null)
+            {
+                throw new ArgumentNullException(nameof(otherAccount));
+            }
+
+            Withdraw(amount);
+            otherAccount.Add(amount);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100000;
+            List<BigInteger> numbers = new List<BigInteger> {1, 1};
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(numbers[numbers.Count - 1] + numbers[numbers.Count - 2]);
+            }
+
+            foreach (BigInteger num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
